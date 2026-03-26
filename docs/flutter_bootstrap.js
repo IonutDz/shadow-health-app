be4c58686833df9a87895527d84c8404391fe3ba
IonutDz <idimasca@nuukmobility.com>
2026-03-26 11:21:00 +0100

    deploy: update web build with Google Sign-In

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3136781157" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1897157619" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

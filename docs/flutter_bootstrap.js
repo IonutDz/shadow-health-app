71ab0db7b08a58500c3ab2ab42f5270a2bf50a3c
IonutDz <idimasca@nuukmobility.com>
2026-03-26 11:28:02 +0100

    deploy: fix Google Sign-In token for web

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "839379671" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4244968127" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

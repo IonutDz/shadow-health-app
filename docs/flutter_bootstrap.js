f4308373e1182d798f51a411b248b0972573da86
IonutDz <idimasca@nuukmobility.com>
2026-03-26 15:54:03 +0100

    deploy: clean bootstrap no broken renderer

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -35,15 +35,8 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae12","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
-
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: ""3328686856" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */"
-  },
-  onEntrypointLoaded: async function(engineInitializer) {
-    const appRunner = await engineInitializer.initializeEngine({
-      renderer: "html"
-    });
-    await appRunner.runApp();
+    serviceWorkerVersion: "4264409941" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
